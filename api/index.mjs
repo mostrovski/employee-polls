8cb90c4f4a67e3d6ccd2f29aeb3da6f7419e046d
mostrovski <ostrovskiime@gmail.com>
2023-04-12 13:24:27 +0200

    Add database and api containers

---
+++ api/index.mjs
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Read environment variables.
+dotenv.config();
+
+// Configure the server.
+const app = express();
+const port = 8080;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Define routes.
+app.get("/", async (req, res) => {
+  return res.json({
+    message:
+      "Connection has been established successfully. Hello from a container!",
+  });
+});
+
+// Start.
+app.listen(port, () =>
+  console.log(`Express is listening! Visit http://localhost:${port}`)
+);
